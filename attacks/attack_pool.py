Fixed successful() possibly propagating gradients. Minor fixes to epsilon_binary_search and uniform_noise.

attacks/attack_pool.py
@@ -7,6 +7,9 @@ import utils
 
 logger = logging.getLogger(__name__)
 
+# Nota: Usa un boolean indexing, ma rimuoverlo richiederebbe riscriverlo in maniera poco
+# elegante. Se viene usato in codice GPU critico, riscriverlo.
+
 class AttackPool(advertorch.attacks.Attack, advertorch.attacks.LabelMixin):
     def __init__(self, predict, evade_detector, pool_attacks, p, targeted=False, clip_min=0, clip_max=1):
         super().__init__(predict, None, clip_min=clip_min, clip_max=clip_max)
@@ -45,7 +48,7 @@ class AttackPool(advertorch.attacks.Attack, advertorch.attacks.LabelMixin):
         best_adversarials = []
 
         for original, pool_result, label in zip(x, pool_results, y):
-            successful = self.successful(pool_result, label)
+            successful = self.successful(pool_result, label).detach()
             assert successful.shape == (len(successful),)
 
             if successful.any():
@@ -76,10 +79,10 @@ class AttackPool(advertorch.attacks.Attack, advertorch.attacks.LabelMixin):
         # Initialization
         if y is None:
             y = self._get_predicted_label(x)
-        
+
         pool_results = torch.stack(
             [attack.perturb(x, y=y, **kwargs).detach() for attack in self.pool_attacks], 1)
-    
+
         assert len(pool_results) == len(x)
         assert pool_results.shape[1] == len(self.pool_attacks)
 

attacks/carlini_wagner.py
@@ -166,7 +166,7 @@ class CarliniWagnerCPULinfAttack(attacks.Attack, attacks.LabelMixin):
                     self.clip_min,
                     self.clip_max).detach()
 
-                successful = self._successful(outputs, y)
+                successful = self._successful(outputs, y).detach()
 
                 if self.return_best:
                     distances = torch.max(
@@ -232,14 +232,14 @@ class CarliniWagnerCPULinfAttack(attacks.Attack, attacks.LabelMixin):
                 y[active],
                 initial_const,
                 taus[active],
-                prev_adversarials[active].clone())
+                prev_adversarials[active].clone()).detach()
 
             # Store the adversarials for the next iteration,
             # even if they failed
             prev_adversarials[active] = adversarials
 
             adversarial_outputs = self.predict(adversarials)
-            successful = self._successful(adversarial_outputs, y[active])
+            successful = self._successful(adversarial_outputs, y[active]).detach()
 
             # If the Linf distance is lower than tau and the adversarial
             # is successful, use it as the new tau
@@ -454,7 +454,7 @@ class CarliniWagnerCUDALinfAttack(attacks.Attack, attacks.LabelMixin):
                     self.clip_min,
                     self.clip_max).detach()
 
-                successful = self._successful(outputs, y)
+                successful = self._successful(outputs, y).detach()
 
                 if self.return_best:
                     distances = torch.max(
@@ -547,7 +547,7 @@ class CarliniWagnerCUDALinfAttack(attacks.Attack, attacks.LabelMixin):
             prev_adversarials = adversarials
 
             adversarial_outputs = self.predict(adversarials)
-            successful = self._successful(adversarial_outputs, y)
+            successful = self._successful(adversarial_outputs, y).detach()
 
             # If the Linf distance is lower than tau and the adversarial
             # is successful, use it as the new tau

cli.py
@@ -41,26 +41,20 @@ def main():
 # TODO: Verificare che attack_comparison_test funzioni ancora
 
 # TODO: Togliere il boolean indexing da uniform_noise.py
-# TODO: Il sistema di return_best è re-implementato in epsilon_binary_search e uniform_noise. Toglierlo?
-# TODO: Finire il debugging di attacks/mip.py e uniform.py
+# TODO: Finire il debugging di attacks/mip.py
 # TODO: Il comando mip è un comando a sé da debuggare!
 
 # TODO: Ri-eseguire l'addestramento? Guardo quale aveva vinto per ogni categoria e prendo quello
 
 # TODO: accuracy.py che fa l'override di dataset per --from-adversarial-dataset non è bellissimo
-# TODO: è giusto che si usi l'inizializzazione intelligente per Brendel? Secondo me no
 # TODO: Droppare completamente il supporto per L2?
 # TODO: Il seeding dovrebbe anche impostare np.random
 # TODO: Parametri dei vari attacchi
-# TODO: Togliere il boolean indexing da attack pool? In teoria non cambia nulla
-# TODO: Debuggare attack_pool.py
 # TODO: Scegliere una misclassification_policy
-# TODO: successful può portare a una propagazione errata dei gradienti
 
 
 # TODO: Cercare i valori corretti di l1 & co. tramite line search?
 # Appunto: CIFAR10-A ha avuto out-of-RAM ed è stato riavviato a partire da 85. è indietro di 35-40 epochs.
-# TODO: Controllare l'attack ratio
 # TODO: Passare a 1-indexing per le epochs nel salvataggio checkpoint?
 
 """