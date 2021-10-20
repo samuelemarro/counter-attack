4d27077ee8b1bbe889b475dbeafa5dcd1f305503
Samuele Marro <marrosamuele@gmail.com>
2021-10-20 13:42:53 +0200

    Removed most architectures.

--- models/cifar.py
+++ models/cifar.py
@@ -1,7 +1,5 @@
 import logging
 
-from collections import OrderedDict
-
 import torch
 import torch.nn as nn
 
@@ -16,15 +14,6 @@ def cifar10_a(masked_relu, num_classes):
         MaskedReLU([8, 15, 15]) if masked_relu else nn.ReLU(),
         nn.Flatten(),
         nn.Linear(1800, num_classes)
-    )
-
-
-def cifar10_b(masked_relu, num_classes):
-    return torch.nn.Sequential(
-        nn.Conv2d(3, 16, 5, stride=4, padding=0),
-        MaskedReLU([16, 7, 7]) if masked_relu else nn.ReLU(),
-        nn.Flatten(),
-        nn.Linear(784, num_classes)
     )
 
 def cifar10_b2(masked_relu, num_classes):
@@ -46,56 +35,16 @@ def cifar10_c(masked_relu, num_classes):
         nn.Linear(72, num_classes)
     )
 
-
-def cifar10_wong_small(masked_relu, num_classes):
-    return torch.nn.Sequential(
-        nn.Conv2d(3, 16, 4, stride=2, padding=1),
-        MaskedReLU([16, 16, 16]) if masked_relu else nn.ReLU(),
-        nn.Conv2d(16, 32, 4, stride=2, padding=1),
-        MaskedReLU([32, 8, 8]) if masked_relu else nn.ReLU(),
-        nn.Flatten(),
-        nn.Linear(32*8*8, 100),
-        MaskedReLU([100]) if masked_relu else nn.ReLU(),
-        nn.Linear(100, num_classes)
-    )
-
-# TODO: masked_relu
-
-
-def cifar10_wong_large():
-    return nn.Sequential(
-        nn.Conv2d(3, 32, 3, stride=1, padding=1),
-        nn.ReLU(),
-        nn.Conv2d(32, 32, 4, stride=2, padding=1),
-        nn.ReLU(),
-        nn.Conv2d(32, 64, 3, stride=1, padding=1),
-        nn.ReLU(),
-        nn.Conv2d(64, 64, 4, stride=2, padding=1),
-        nn.ReLU(),
-        nn.Flatten(),
-        nn.Linear(64*8*8, 512),
-        nn.ReLU(),
-        nn.Linear(512, 512),
-        nn.ReLU(),
-        nn.Linear(512, 10)
-    )
-
 def cifar10(architecture, masked_relu, num_classes=10, pretrained=None):
     if pretrained:
         raise NotImplementedError()
 
     if architecture == 'a':
         model = cifar10_a(masked_relu, num_classes)
-    elif architecture == 'b':
-        model = cifar10_b(masked_relu, num_classes)
     elif architecture == 'b2':
         model = cifar10_b2(masked_relu, num_classes)
     elif architecture == 'c':
         model = cifar10_c(masked_relu, num_classes)
-    elif architecture == 'wong_small':
-        model = cifar10_wong_small(masked_relu, num_classes)
-    elif architecture == 'wong_large':
-        model = cifar10_wong_large()
     else:
         raise ValueError(
             f'Architecture "{architecture}" not supported for CIFAR10.')
--- models/mnist.py
+++ models/mnist.py
@@ -15,53 +15,6 @@ def mnist_a(masked_relu, num_classes):
         nn.Linear(100, num_classes)
     )
 
-
-def mnist_b(masked_relu, num_classes):
-    return nn.Sequential(
-        nn.Flatten(),
-        nn.Linear(784, 24),
-        torch_utils.MaskedReLU([24]) if masked_relu else nn.ReLU(),
-        nn.Linear(24, 24),
-        torch_utils.MaskedReLU([24]) if masked_relu else nn.ReLU(),
-        nn.Linear(24, num_classes)
-    )
-
-def mnist_b2(masked_relu, num_classes):
-    return nn.Sequential(
-        nn.Flatten(),
-        nn.Linear(784, 200),
-        torch_utils.MaskedReLU([200]) if masked_relu else nn.ReLU(),
-        nn.Linear(200, num_classes)
-    )
-
-def mnist_b3(masked_relu, num_classes):
-    return nn.Sequential(
-        nn.Conv2d(1, 4, 5, stride=4, padding=0),
-        torch_utils.MaskedReLU([4, 6, 6]) if masked_relu else nn.ReLU(),
-        nn.Flatten(),
-        nn.Linear(144, num_classes)
-    )
-
-def mnist_b4(masked_relu, num_classes):
-    return nn.Sequential(
-        nn.Flatten(),
-        nn.Linear(784, 100),
-        torch_utils.MaskedReLU([100]) if masked_relu else nn.ReLU(),
-        nn.Linear(100, 100),
-        torch_utils.MaskedReLU([100]) if masked_relu else nn.ReLU(),
-        nn.Linear(100, num_classes)
-    )
-
-def mnist_b5(masked_relu, num_classes):
-    return nn.Sequential(
-        nn.Flatten(),
-        nn.Linear(784, 64),
-        torch_utils.MaskedReLU([64]) if masked_relu else nn.ReLU(),
-        nn.Linear(64, 64),
-        torch_utils.MaskedReLU([64]) if masked_relu else nn.ReLU(),
-        nn.Linear(64, num_classes)
-    )
-
 def mnist_b6(masked_relu, num_classes):
     return nn.Sequential(
         nn.Conv2d(1, 4, 5, stride=3, padding=0),
@@ -85,16 +38,6 @@ def mnist(architecture, masked_relu, num_classes=10, pretrained=None):
 
     if architecture == 'a':
         model = mnist_a(masked_relu, num_classes)
-    elif architecture == 'b':
-        model = mnist_b(masked_relu, num_classes)
-    elif architecture == 'b2':
-        model = mnist_b2(masked_relu, num_classes)
-    elif architecture == 'b3':
-        model = mnist_b3(masked_relu, num_classes)
-    elif architecture == 'b4':
-        model = mnist_b4(masked_relu, num_classes)
-    elif architecture == 'b5':
-        model = mnist_b5(masked_relu, num_classes)
     elif architecture == 'b6':
         model = mnist_b6(masked_relu, num_classes)
     elif architecture == 'c':
