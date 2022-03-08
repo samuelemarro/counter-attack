296c508ba84f1fb9e905a97ffbf6bdf2af475fae
Samuele Marro <marrosamuele@gmail.com>
2022-03-08 19:29:24 +0100

    Added extra attack batches.

---
+++ extra_attack_batching/batch-0.sh
@@ -0,0 +1,3 @@
+./run_extra.sh mnist a standard carlini 7215
+./run_extra.sh mnist a adversarial carlini 7215
+./run_extra.sh mnist a relu carlini 7215
---
+++ extra_attack_batching/batch-11.sh
@@ -0,0 +1,2 @@
+./run_extra.sh mnist c adversarial carlini 6342
+./run_extra.sh mnist c relu carlini 6342
