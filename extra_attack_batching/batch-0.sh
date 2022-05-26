0f883efa2d7182749caad25dd7cbd500d6998375
Samuele Marro <marrosamuele@gmail.com>
2022-05-26 09:46:20 +0200

    Removed more old files.

--- extra_attack_batching/batch-0.sh
+++
@@ -1,3 +0,0 @@
-./run_extra.sh mnist a standard carlini 7215
-./run_extra.sh mnist a adversarial carlini 7215
-./run_extra.sh mnist a relu carlini 7215
--- extra_attack_batching/batch-11.sh
+++
@@ -1,2 +0,0 @@
-./run_extra.sh mnist c adversarial carlini 6342
-./run_extra.sh mnist c relu carlini 6342
